2b3fe92e58fa41c7e241162b26911e21f098663a
Salva Carsi Moreno <salvavcarsi@gmail.com>
2024-03-05 15:38:01 +0100

    flat list

--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -1,25 +1,26 @@
-import { View, Text } from "react-native"
+import { View, Text, FlatList } from 'react-native'
 
-import repositories from "../data/repositories.js"
+import repositories from '../data/repositories.js'
 
 const RepositoryList = () => {
   return (
-    <View>
-      {repositories.map((repo) => {
+    <FlatList
+      data={repositories}
+      renderItem={({ item }) => {
         return (
-          <View key={repo.id}>
-            <Text>id: {repo.id}</Text>
-            <Text>Fullname: {repo.fullName}</Text>
-            <Text>Description: {repo.description}</Text>
-            <Text>Language: {repo.language}</Text>
-            <Text>Stars: {repo.stargazersCount}</Text>
-            <Text>Forks: {repo.forksCount}</Text>
-            <Text>Review: {repo.reviewCount}</Text>
-            <Text>Rating: {repo.ratingAverage}</Text>
+          <View key={item.id}>
+            <Text>id: {item.id}</Text>
+            <Text>Fullname: {item.fullName}</Text>
+            <Text>Description: {item.description}</Text>
+            <Text>Language: {item.language}</Text>
+            <Text>Stars: {item.stargazersCount}</Text>
+            <Text>Forks: {item.forksCount}</Text>
+            <Text>Review: {item.reviewCount}</Text>
+            <Text>Rating: {item.ratingAverage}</Text>
           </View>
         )
-      })}
-    </View>
+      }}
+    />
   )
 }
 
