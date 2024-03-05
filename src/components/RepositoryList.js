9051106e1ebcd4684b8541b90da2c6de59fcb66b
Salva Carsi Moreno <salvavcarsi@gmail.com>
2024-03-05 13:13:11 +0100

    eslint

---
+++ src/components/RepositoryList.js
@@ -0,0 +1,26 @@
+import { View, Text } from "react-native";
+
+import repositories from "../data/repositories.js";
+
+const RepositoryList = () => {
+  return (
+    <View>
+      {repositories.map((repo) => {
+        return (
+          <View key={repo.id}>
+            <Text>id: {repo.id}</Text>
+            <Text>Fullname: {repo.fullName}</Text>
+            <Text>Description: {repo.description}</Text>
+            <Text>Language: {repo.language}</Text>
+            <Text>Stars: {repo.stargazersCount}</Text>
+            <Text>Forks: {repo.forksCount}</Text>
+            <Text>Review: {repo.reviewCount}</Text>
+            <Text>Rating: {repo.ratingAverage}</Text>
+          </View>
+        );
+      })}
+    </View>
+  );
+};
+
+export default RepositoryList;
