01b080e9f3012832cd732bd546d3d1dc3186d88d
Salva Carsi Moreno <salvavcarsi@gmail.com>
2024-03-06 17:01:28 +0100

    add react router native

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,20 @@
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
+import { NativeRouter, Route, Routes } from 'react-router-native'
 
 import AppBar from './AppBar'
 import RepositoryList from './RepositoryList'
 
 const Main = () => {
   return (
-    <View style={{ flex: 1, paddingBottom: 40 }}>
-      <AppBar />
-      <RepositoryList />
-    </View>
+    <NativeRouter>
+      <View style={{ flex: 1, paddingBottom: 40 }}>
+        <AppBar />
+        <Routes>
+          <Route path="/" exact element={<RepositoryList />} />
+          <Route path="/signin" exact element={<Text>Working on it!</Text>} />
+        </Routes>
+      </View>
+    </NativeRouter>
   )
 }
 
