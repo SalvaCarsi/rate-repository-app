e65203822ce37a8a6eef572dec83343cbaa525ce
Salva Carsi Moreno <salvavcarsi@gmail.com>
2024-03-12 11:54:42 +0100

    query apollo serveer

--- src/components/RepositoryList.js
+++ src/components/RepositoryList.js
@@ -1,12 +1,53 @@
+import { useState, useEffect } from 'react'
 import { Text, FlatList } from 'react-native'
 
 import RepositoryItem from './RepositoryItem'
-import repositories from '../data/repositories.js'
 
 const RepositoryList = () => {
+  const [repositories, setRepositories] = useState(null)
+  const fetchRepositories = async () => {
+    fetch('http://localhost:4000/graphql', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `query Repositories {
+                    repositories {
+                      edges {
+                        node {
+                          fullName
+                          id
+                          language
+                          name
+                          ownerAvatarUrl
+                          ownerName
+                          ratingAverage
+                          reviewCount
+                      }
+                    }
+                  }
+                }`,
+        variables: {},
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setRepositories(result.data)
+      })
+  }
+
+  useEffect(() => {
+    fetchRepositories()
+  }, [])
+
+  const repositoriesNodes = repositories
+    ? repositories?.repositories?.edges?.map((edge) => edge.node)
+    : []
+
   return (
     <FlatList
-      data={repositories}
+      data={repositoriesNodes}
       ItemSeparatorComponent={() => <Text> </Text>}
       renderItem={({ item }) => {
         return <RepositoryItem item={item} />
